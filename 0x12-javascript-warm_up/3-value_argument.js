73c4e53fad93819828d97f94b0c72837ee2807cb
Afooseyi <afooseyi@gmail.com>
2023-03-13 08:50:39 -0700

    a script that prints the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2])o
-t@cd0b0e875123:/alx-higher_level_programming/0x12-javascript-warm_up# vi
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
