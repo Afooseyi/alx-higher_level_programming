af5efbb0ec5e88abe7128583df510e05b317671a
Afooseyi <afooseyi@gmail.com>
2023-03-13 08:17:46 -0700

    a script that prints a square

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,3 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2])o
+t@cd0b0e875123:/alx-higher_level_programming/0x12-javascript-warm_up# vi
